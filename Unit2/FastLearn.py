c4320fc7787a98ba2d56651827f6f58ef4a4fc72
chenwu <chenwu@meitunmama.com>
2018-12-14 17:52:28 +0800

    提交第二章的学习

--- Unit2/FastLearn.py
+++ Unit2/FastLearn.py
@@ -3,3 +3,14 @@
 aList=[1,2,3,4]
 bList = aList[2:]
 print(bList)
+bList=aList[:3]
+print(bList)
+aTuple=('robots',77,93,'try')
+print(aTuple[:2])
+aDict={'port':8080,'ip':'222.3'}
+print(aDict['port'])
+# for key in aDict:
+#     print key,aDict[key]
+x=3
+# if x<0:
+#     print "x must at least 0"
