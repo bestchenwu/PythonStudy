cde08bfd40e5c467137edfd6024b5b2e1c638c81
bestchenwu <bestchenwu@163.com>
2018-12-24 19:24:16 +0800

    提交第五章的学习

---
+++ Unit5/TestExamination.py
@@ -0,0 +1,11 @@
+score=input("please input your score:")
+#print(type(score))
+score=int(score)
+print(score)
+if score>=90 and score<=100:
+    print("score is A")
+elif score>=80 and score<90:
+    print("score is B")
+else:
+    print("score is C")
+
