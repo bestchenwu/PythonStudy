3b05d80717534545a3a9e303aa3fcce95a0568bc
bestchenwu <bestchenwu@163.com>
2019-01-05 23:29:52 +0800

    提交numpy的学习

--- Numpy/study/NumpyTest.py
+++ Numpy/study/NumpyTest.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
-array1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+# array1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 # print(array1.shape)
 # print(array1[1][2])
 # # 1 means rows,:means columns
@@ -14,5 +15,31 @@ array1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 # # [2]
 # print(y)
 # axis means
-array2 = array1.sum(axis=0)
-print(array2)
+# array2 = array1.sum(axis=0)
+# print(array2)
+# array3 = np.zeros(5)
+# print(array3)
+# array4 = np.random.rand(5)
+# print(array4)
+# array5 = np.ones((2, 4))
+# print(array5)
+#array1 =
+# [1,2
+#  3,4]
+array1 = np.array([[1, 2], [3, 4]])
+# array2=
+# [5,6
+#   7,8]
+array2 = np.array([[5, 6], [7, 8]])
+# print sum
+print(array1+array2)
+print(array1-array2)
+print(array1/array2)
+# 需要注意矩阵直接用乘法代表元素逐项相乘
+# [[1*5] [2*6]
+#  [3*7] [4*8]]
+print(array1*array2)
+# 矩阵乘法运算的方式是用dot,公式为:
+# [[ 1*5+2*7,1*6+2*8]
+#  [5*3+4*7],3*6+4*8 ]
+print(array1.dot(array2))
